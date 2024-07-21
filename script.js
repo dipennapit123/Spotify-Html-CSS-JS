bdeba6ee37a54748b7d0e940e3cfa37c9bf5606c
dipennapit123 <dipennapit45@gmail.com>
2024-07-21 21:54:19 +0545

    gif animation and music playlist

--- script.js
+++ script.js
@@ -2,44 +2,66 @@ let songIndex = 0;
 let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+let songItems=Array.from(document.getElementsByClassName("songName"));
 
 let songs = [
   {
-    songName: "Salem-e-ishq",
+    songName: "I Love You",
     filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    coverPath: "covers/2.jpg",
   },
   {
-    songName: "Salem-e-ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    songName: "I love you too",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/2.jpg",
   },
   {
-    songName: "Salem-e-ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    songName: "Date Night",
+    filePath: "songs/3.mp3",
+    coverPath: "covers/3.jpg",
   },
   {
-    songName: "Salem-e-ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    songName: "Lets have fun tonight",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/4.jpg",
   },
   {
-    songName: "Salem-e-ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    songName: "Lets have fun tonight",
+    filePath: "songs/5.mp3",
+    coverPath: "covers/5.jpg",
   },
   {
-    songName: "Salem-e-ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    songName: "Go go brother todays is the night",
+    filePath: "songs/6.mp3",
+    coverPath: "covers/6.jpg",
   },
   {
-    songName: "Salem-e-ishq",
-    filePath: "songs/1.mp3",
-    coverPath: "covers/1.jpg",
+    songName: "Yeah Finally I Scored",
+    filePath: "songs/7.mp3",
+    coverPath: "covers/7.jpg",
+  },
+  {
+    songName: "No Lets have fun only",
+    filePath: "songs/8.mp3",
+    coverPath: "covers/8.jpg",
+  },
+  {
+    songName: "No Im serious",
+    filePath: "songs/9.mp3",
+    coverPath: "covers/9.jpg",
+  },
+  {
+    songName: "Lets get married",
+    filePath: "songs/10.mp3",
+    coverPath: "covers/10.jpg",
   },
 ];
+songItems.forEach((element,i)=>{
+    console.log(element,i);
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+
+})
 
 //Handle play/pause click
 masterPlay.addEventListener("click", () => {
@@ -47,19 +69,24 @@ masterPlay.addEventListener("click", () => {
     audioElement.play();
     masterPlay.classList.remove("fa-circle-play");
     masterPlay.classList.add("fa-circle-pause");
-}
-else{
-      audioElement.pause();
-      masterPlay.classList.remove("fa-circle-pause");
-      masterPlay.classList.add("fa-circle-play");
-
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-circle-pause");
+    masterPlay.classList.add("fa-circle-play");
+    gif.style.opacity = 0;
   }
 });
 
 //Listen to  Events
-myProgressBar.addEventListener("timeupdate", () => {
-  console.log('timeupdate')
- 
-
+audioElement.addEventListener("timeupdate", () => {
   //Update Seekbar
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  console.log(progress);
+  myProgressBar.value = progress;
+});
+
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
 });
